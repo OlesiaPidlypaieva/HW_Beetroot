61a88ddc071f1719b1ce3ec7ae9c6a2414795661
OlesiaPidlypaieva <Vorobyshe4ek@gmail.com>
2022-10-21 23:40:56 +0300

    final

--- Exam_2/src/js/index.js
+++ Exam_2/src/js/index.js
@@ -1,127 +1,108 @@
-//fancybox
+//burger
 
-Fancybox.bind('[data-fancybox="gallery"]', {
-   caption: function (fancybox, carousel, slide) {
-      return (
-         `${slide.index + 1} / ${carousel.slides.length} <br />` + slide.caption
-      );
-   },
+let navigationBurger = document.querySelector('.navigation__burger');
+let navigationBurgerWrapper = document.querySelector('.navigation__wrapper-burger');
+let navigationBurgerLink = document.querySelectorAll('.navigation__link2');
+
+navigationBurger.addEventListener('click', function(){
+   navigationBurger.classList.toggle('active');
+   navigationBurgerWrapper.classList.toggle('active'); 
+})
+
+navigationBurgerLink.forEach (function(item){
+   item.addEventListener('click', function(){
+      navigationBurger.classList.remove('active');
+      navigationBurgerWrapper.classList.remove('active'); 
+})
 });
 
-//swiper
-//main swiper
-const pageSwiper = new Swiper('#pageSwiper', {
-   wrapperClass: "page__wrapper",
-   slideClass: "page__screen",
-   direction: 'vertical',
-   slidesPerView: 'auto',
-
-   keyboard: {
-      enabled:true,
-      onlyInViewport:true,
-      pageUpDown:true,
-   },
-
-   mousewheel: {
-      sensitivity:1,
-   },
-
-   watchOverflow:true,
-   speed:900,
-   observer:true,
-   observeParents:true,
-   observeSlideChildren:true,
-
-   pagination: {
-      el: '.page__pagination',
-      type: 'bullets',
-      clickable:true,
-      bulletClass: 'page__bullet',
-      bulletActiveClass: 'page__bullet_active',
-   },
-
-   init:false,
-
-   on: {
-      init: function(){
-         menuSlider();
-      },
-
-      slideChange: function(){
-         menuSliderRemove()
-         menuLinks [pageSwiper.realIndex].classList.add('active');
+//main swiper function
+(function () {
+   const breakpoint = window.matchMedia('(max-width:1023px)');
+   let pageSwiper; 
+   //resize 
+   const breakpointChecker = function () {
+      if (breakpoint.matches === true) {
+      if (pageSwiper!== undefined) pageSwiper.destroy(true, true); 
+      return; 
+      } else if (breakpoint.matches === false) {
+      return loadSwiper();
       }
-   },
-});
-
-
-let pagination = $('.page__pagination');
-let bullet = $('.page__bullet');
-let screen = $('.page__screen');
-
-
-$(document).on('mousewheel', screen, function(){
-   if ((bullet.attr('aria-label') == "Go to slide 1") && (bullet.attr('aria-current') == "true")) {
-      pagination.toggleClass('light');
-   } else {
-      pagination.removeClass('light');
-   }
-}
-)
-
-$(document).ready(function() {
-   if ((bullet.attr('aria-label') == "Go to slide 1") && (bullet.attr('aria-current') == "true")) {
-   pagination.toggleClass('light');
-   }
-})
-
-//header links
-
-let menuLinks = document.querySelectorAll('.navigation__link');
-
-function menuSlider(){
-   if(menuLinks.lenth > 0) {
-      menuLinks [pageSwiper.realIndex].classList.add('active');
-      for (let index = 0; index < menuLinks.lenth; index++) {
-         const menuLink = menuLinks[index];
-         menuLink.addEventListener("click", function (e) {
-            menuSliderRemove();
-            pageSwiper.slideTo(index , 800);
-            menuLink.classList.add('active')
-            e.preventDefault();
-         })
-      }
-   }
-}
-
-function menuSliderRemove() {
-   let menuLinkActive = document.querySelectorAll('.navigation__link.active');
-   if(menuLinkActive) {
-      menuLinkActive.classList.remove('active');
-   }
-}
-
-pageSwiper.init();
-
-//swiper news
-
-const swiper = new Swiper('.swiper', {
-   loop: true,
-   slidesPerView: '3',
-   pagination: {
-      el: '.swiper-pagination',
-   },
-});
-
-const swiperPrev = document.getElementById('swiperPrev')
-const swiperNext = document.getElementById('swiperNext')
-
-swiperPrev.addEventListener('click', () => {
-   swiper.slidePrev();
-})
-swiperNext.addEventListener('click', () => {
-   swiper.slideNext();
-})
+   };
+      //swiper
+      const loadSwiper = function () {
+         pageSwiper = new Swiper('#pageSwiper', {
+            wrapperClass: "page__wrapper",
+            slideClass: "page__screen",
+            direction: 'vertical',
+            slidesPerView: 'auto',
+         
+            keyboard: {
+               enabled:true,
+               onlyInViewport:true,
+               pageUpDown:true,
+            },
+         
+            mousewheel: {
+               sensitivity:1,
+            },
+         
+            watchOverflow:true,
+            speed:900,
+            observer:true,
+            observeParents:true,
+            observeSlideChildren:true,
+         
+            pagination: {
+               el: '.page__pagination',
+               type: 'bullets',
+               clickable:true,
+               bulletClass: 'page__bullet',
+               bulletActiveClass: 'page__bullet_active',
+            },
+         
+            init:false,
+         
+            on: {
+               init: function () {
+                  menuSlider();
+               },
+               slideChange: function () {
+                  menuSliderRemove();
+                  menuLinks[pageSwiper.realIndex].classList.add('_active');
+               },
+            },
+         });
+         // header links
+   
+         let menuLinks = document.querySelectorAll('.navigation__link');
+   
+         function menuSlider() {
+            if (menuLinks.length > 0) {
+               menuLinks[pageSwiper.realIndex].classList.add('_active');
+               for (let index = 0; index < menuLinks.length; index++) {
+                  const menuLink = menuLinks[index];
+                  menuLink.addEventListener("click", function (e) {
+                     menuSliderRemove();
+                     pageSwiper.slideTo(index, 800);
+                     menuLink.classList.add('_active');
+                     e.preventDefault();
+                  });
+               }
+            }
+         }
+   
+         function menuSliderRemove() {
+            let menuLinkActive = document.querySelector('.navigation__link._active');
+            if (menuLinkActive) {
+               menuLinkActive.classList.remove('_active');
+            }
+         };
+         pageSwiper.init();
+   }; 
+      breakpoint.addListener(breakpointChecker);
+      breakpointChecker();
+   })();
 
 //animation
 
@@ -133,12 +114,70 @@ const wow = new WOW({
 })
 wow.init(); 
 
+
+//swiper news
+
+const swiper = new Swiper('.swiper', {
+   loop: true,
+   pagination: {
+      el: '.swiper-pagination',
+   },
+
+   autoplay: {
+      delay:2000,
+      pauseOnMouseEnter: true,
+      disableOnInteraction: false,
+   },
+
+   breakpoints: {
+
+      320: {
+         slidesPerView: 1,
+         spaceBetween: 30,
+      },
+
+      850: {
+         slidesPerView: 2,
+         spaceBetween: 30,
+      },
+
+      1420: {
+         slidesPerView: 3,
+         spaceBetween: 30,
+      }
+   }
+});
+
+const swiperPrev = document.querySelector('#swiperPrev')
+const swiperNext = document.querySelector('#swiperNext')
+
+swiperPrev.addEventListener('click', function()  {
+   swiper.slidePrev();
+})
+swiperNext.addEventListener('click', function() {
+   swiper.slideNext();
+})
+
+//fancybox
+
+Fancybox.bind('[data-fancybox="gallery"]', {
+   caption: function (fancybox, carousel, slide) {
+      return (
+         `${slide.index + 1} / ${carousel.slides.length} <br />` + slide.caption
+      );
+   },
+});
+
 //maps
 
 function Map() {
    let mapProp = {
       zoom:13,
       center: {lat: 40.670068, lng:-73.8974411778513},
+      streetViewControl: false,
+      scrollwheel: false,
+      mapTypeControl: false,
+
       styles: [
          {
             "elementType": "geometry",
@@ -386,7 +425,7 @@ function Map() {
             }
          ]       
    };
-   let map = new google.maps.Map(document.getElementById('map'),mapProp); 
+   let map = new google.maps.Map(document.querySelector('#map'),mapProp); 
    new google.maps.Marker ({
       position: {lat: 40.678872, lng: -73.896542},
       map,
@@ -398,50 +437,48 @@ function Map() {
 //form
 
 document.addEventListener('DOMContentLoaded', function(){
-   const form = document.getElementById('form')
+   const form = document.querySelector('#form')
    form.addEventListener('submit', function (e){
       e.preventDefault();
       let error = formValidate(form);
-
       if (error === 0) {
-            alert('Our manager will contact you soon!')
+            alert('Our manager will contact you soon!');
       } else {
-            alert('Please, fill required filds...')
+            alert('Please, fill required filds...');
             }
    });
 
-   function formValidate (form) {
-      let error = 0;
-      let formReq = document.querySelectorAll('.req');
-		for (let index = 0; index < formReq.length; index++) {
-			const input = formReq[index];
-			formRemoveError(input);
-
-			if (input.classList.contains('email')) {
-				if (emailTest(input)) {
-					formAddError(input);
-					error++;
-				}
-			} else {
-				if (input.value === '') {
-					formAddError(input);
-					error++;
-				}
-			}
-		}
-		return error;
+function formValidate (form) {
+   let error = 0;
+   let formReq = document.querySelectorAll('.req');
+   for (let index = 0; index < formReq.length; index++) {
+      const input = formReq[index];
+      formRemoveError(input);
+      if (input.classList.contains('email')) {
+         if (emailTest(input)) {
+            formAddError(input);
+            error++;
+         }
+      } else {
+         if (input.value === '') {
+            formAddError(input);
+            error++;
+         }
+      }
+   }
+   return error;
 }
 
-   function formAddError(input) {
-		input.parentElement.classList.add('_error');
-		input.classList.add('_error');
-	}
-	function formRemoveError(input) {
-		input.parentElement.classList.remove('_error');
-		input.classList.remove('_error');
-	}
+function formAddError(input) {
+   input.parentElement.classList.add('_error');
+   input.classList.add('_error');
+}
+function formRemoveError(input) {
+   input.parentElement.classList.remove('_error');
+   input.classList.remove('_error');
+}
 
-   function emailTest(input) {
-		return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
-	}
+function emailTest(input) {
+   return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+}
 });
